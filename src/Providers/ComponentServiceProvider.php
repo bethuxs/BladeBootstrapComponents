9ac73f1b07a9deba6fb700d6786c69085a8c1baa
Alberto Sanchez <alberroteran@gmail.com>
2024-12-15 12:37:23 -0300

    Fixed component namespace

--- src/Providers/ComponentServiceProvider.php
+++ src/Providers/ComponentServiceProvider.php
@@ -9,7 +9,13 @@ class ComponentServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'bs');
-        Blade::componentNamespace('\\Bethuxs\BladeBootstrapComponents\Components', 'bs');
+        Blade::componentNamespace('Bethuxs\\BladeBootstrapComponents\\Components', 'bs');
+        // for the flash messages
+        \Spatie\Flash\Flash::levels([
+            'success' => 'alert-success',
+            'warning' => 'alert-warning',
+            'error' => 'alert-error',
+        ]);
     }
 
     public function register()

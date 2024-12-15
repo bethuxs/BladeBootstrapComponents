f4f164f92f61dd46d94478c12013cf3f4ea95660
Alberto Sanchez <alberroteran@gmail.com>
2024-12-15 12:37:56 -0300

    Added flash to form

--- src/resources/views/components/form.blade.php
+++ src/resources/views/components/form.blade.php
@@ -1,4 +1,5 @@
 <form {{ $attributes }} @if($attributes->get('route')) action="{{ route($attributes->get('route')) }}" @endif>
+  <x-bs::flash />
   @if($attributes->get('method') != 'get')
     @csrf
   @endif

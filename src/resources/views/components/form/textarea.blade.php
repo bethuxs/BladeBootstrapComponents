0fb6daa1876f731745ab6768c7e0a58f3efbafdd
Alberto Sanchez <alberroteran@gmail.com>
2025-07-26 22:24:02 -0300

    Fixed value

--- src/resources/views/components/form/textarea.blade.php
+++ src/resources/views/components/form/textarea.blade.php
@@ -1,4 +1,4 @@
-<textarea {{ $attributes->class(['form-control', 'is-invalid' => $errors->has($name)]) }} value="{{ old($name, $value) }}" name="{{$name}}"></textarea>
+<textarea {{ $attributes->class(['form-control', 'is-invalid' => $errors->has($name)]) }} name="{{$name}}">{{ old($name, $value) }}</textarea>
 
 @error($name)
   <div class="invalid-feedback">{{ $message }}</div>
